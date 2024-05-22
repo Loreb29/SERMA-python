1d62051ddbd6fdbe0ef7b30df41f7d1db79e4cc7
Loreb29 <juanda2915@gmail.com>
2024-05-22 09:23:45 -0500

    Interfaz basica

--- Main.py
+++ Main.py
@@ -17,16 +17,13 @@ bienvenido = customtkinter.CTkLabel(app,text="Bienvenido a SERMA", font=("Helvet
 ImageHuellaCarga = customtkinter.CTkImage(light_image=Image.open("image\\HuellaCarga.png"),size=(150,150))
 huellaCargalabel = customtkinter.CTkLabel(app, text="" , image=ImageHuellaCarga, height=50, width=50)
 botonIniciar = customtkinter.CTkButton(master = app, text="Iniciar")
-
 stringBuscador = ""
 buscador = customtkinter.CTkEntry(app,corner_radius=50,textvariable=stringBuscador, width=200 )
-
 contador = 1 + 0
 
 def entras():
     botonIniciar.place_forget()
     huellaCargalabel.place_forget()
-
     if(contador == 2):
         alerta = customtkinter.CTkToplevel()
 
@@ -34,55 +31,26 @@ def entras():
                                                 command= lambda: alerta.wm_forget)
         boto.pack(pady=10)
     else:
-        Bienvenido()
+        inicioProgreso()
             
     
 t = threading.Timer(3, entras)
 
 
-def validacion():
-     if contra.get()== "1234":
-          Bienvenido()
+#def validacion():
+#     if contra.get()== "1234":
+#          Bienvenido()
 
-def Bienvenido():
-
-    
-    print("si")
-    inicio.place_forget()
-    contraseña.place_forget()
-    Contraseñalabel.place_forget()
-    button.place_forget()
-    logolabel.place_forget()
-    huellabutton.place_forget()
-
-    
-    bienvenido.place(relx=0.38, rely=0.4)
-    f = threading.Timer(3, escritorio)
-    f.start()
-
-
-def inicioHuella():
-    
-    contraseña.place_forget()
-    Contraseñalabel.place_forget()
-    button.place_forget()
-    global botonIniciar
-    botonIniciar._command = inicioProgreso
-    if(contador == 1):
-        
-        botonIniciar.place(relx=0.45, y=710)
-    else:
-        print("Vamos")
-        botonIniciar.place_forget()
-        botonIniciar.destroy()
-        botonIniciar = customtkinter.CTkButton(master = app, text="Intentar nuevamente",command = inicioProgreso)
-        botonIniciar.place(relx=0.45, y=710)
-   
-    
-   
-
-    
-
+#def Bienvenido():
+#    print("si")
+#    inicio.place_forget()
+#    contraseña.place_forget()
+#    Contraseñalabel.place_forget()
+#    button.place_forget()
+#    logolabel.place_forget()
+#    bienvenido.place(relx=0.38, rely=0.4)
+#    f = threading.Timer(3, escritorio)
+#    f.start()    
 
 
 def inicioProgreso():
@@ -96,34 +64,43 @@ def inicioProgreso():
 
 
 
-inicio = customtkinter.CTkLabel(app,text="Nicolas", font=("Helvetica",38), fg_color='transparent')
-inicio.place(relx=0.45, y=150)
+inicio = customtkinter.CTkLabel(app,text="Inicio de sesión\nusuario", font=("Helvetica",38), fg_color='transparent')
+inicio.place(relx=0.06, y=150)
 
+iniciodoc = customtkinter.CTkLabel(app,text="Inicio de sesión\ndocente", font=("Helvetica",38), fg_color='transparent')
+iniciodoc.place(relx=0.76, y=150)
 
 ImageUser = customtkinter.CTkImage(light_image=Image.open("image\\Navarrete.png"),size=(300,300))
 logolabel = customtkinter.CTkLabel(app, text="" , image=ImageUser, height=30, width=30)
-logolabel.place(relx=0.4, y=200)
 
 ImageHuella = customtkinter.CTkImage(light_image=Image.open("image\\Huella.png"),size=(50,50))
-huellabutton = customtkinter.CTkButton(app, text="" , image=ImageHuella, height=30, width=30, command=inicioHuella)
-huellabutton.place(relx=0.9, y=700)
-
 
 user = customtkinter.StringVar(app)
-contra = customtkinter.StringVar(app)
+#contra = customtkinter.StringVar(app)
 
 
 Contraseñalabel = customtkinter.CTkLabel(app, text="Contraseña", font=("Helvetica",24) )
-Contraseñalabel.place(relx=0.4, y=550)
+#Contraseñalabel.place(relx=0.4, y=550)
 
 
-contraseña = customtkinter.CTkEntry(app, placeholder_text="Ingresa tu contraseña",corner_radius=50,show="*",textvariable=contra )
-contraseña.place(relx=0.5, y=550)
+#contraseña = customtkinter.CTkEntry(app, placeholder_text="Ingresa tu contraseña",corner_radius=50,show="*",textvariable=contra )
+#contraseña.place(relx=0.5, y=550)
+
+def pantallainicio():
+    button.place_forget()
+    buttondoc.place_forget()
+    labelSERMA.place_forget()
+
+button = customtkinter.CTkButton(master = app, text="Ingresar", command=pantallainicio)
+button.place(relx=0.1, y=600)
 
 
-button = customtkinter.CTkButton(master = app, text="Ingresar", command=validacion)
-button.place(relx=0.45, y=600)
 
+buttondoc = customtkinter.CTkButton(master = app, text="Ingresar", command=inicioProgreso)
+buttondoc.place(relx=0.8, y=600)
+ImgSERMA = customtkinter.CTkImage(light_image=Image.open("image\\SERMA.png"),size=(300,300))
+labelSERMA = customtkinter.CTkLabel(app, text="" , image=ImgSERMA, height=30, width=30)
+labelSERMA.place(relx=0.38, y=150)
 
 def escritorio():
     bienvenido.place_forget()
